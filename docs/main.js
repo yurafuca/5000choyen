b9f94c4c4455091ba0aaabcc8d155ca74f571f0a
amagomori <madokawaii@gmail.com>
2017-06-09 14:02:42 +0900

    Change text style

--- docs/main.js
+++ docs/main.js
@@ -4,7 +4,7 @@ window.onload = function () {
   canvas = document.getElementById("canvas");
   textbox = document.getElementById("textbox");
   ctx = canvas.getContext('2d');
-  ctx.font = '100px mplus-1p-black';
+  ctx.font = '100px ipaexm';
   ctx.lineJoin = 'round';
   ctx.setTransform(1,0,-0.4,1,0,0);
 };
@@ -40,10 +40,10 @@ function redraw() {
   //金色
   {
     var grad = ctx.createLinearGradient(0, 20, 0, 100);
-    grad.addColorStop(0.3, 'rgb(255, 255, 255)');
-    grad.addColorStop(0.5, 'rgb(240, 180, 5)');
-    grad.addColorStop(0.8, 'rgb(89, 33, 0)');
-    grad.addColorStop(1, 'rgb(240, 180, 5)');
+    grad.addColorStop(0.3, 'rgb(214, 220, 227)');
+    grad.addColorStop(0.5, 'rgb(218, 224, 232)');
+    grad.addColorStop(0.8, 'rgb(207, 207, 209)');
+    grad.addColorStop(1, 'rgb(127,127,128)');
     ctx.strokeStyle = grad;
     ctx.lineWidth = 19;
     ctx.strokeText(text, posx, posy);
@@ -51,16 +51,20 @@ function redraw() {
 
   //白
   ctx.lineWidth = 6;
-  ctx.strokeStyle = '#FFFFFF';
+  ctx.strokeStyle = '#10193A';
   ctx.strokeText(text, posx, posy - 3);
 
   //赤
   {
     var grad = ctx.createLinearGradient(0, 20, 0, 100);
-    grad.addColorStop(0, 'rgb(230, 0, 0)');
-    grad.addColorStop(0.5, 'rgb(123, 0, 0)');
-    grad.addColorStop(0.51, 'rgb(240, 0, 0)');
-    grad.addColorStop(1, 'rgb(5, 0, 0)');
+    grad.addColorStop(0, 'rgb(192,204,217)');
+    grad.addColorStop(0.05, 'rgb(212,220,228)');
+    grad.addColorStop(0.15, 'rgb(241,242,243)');
+    grad.addColorStop(0.23, 'rgb(241,242,243)');
+    grad.addColorStop(0.5, 'rgb(190,190,190)');
+    grad.addColorStop(0.51, 'rgb(220,220,221)');
+    grad.addColorStop(0.52, 'rgb(220,220,220)');
+    grad.addColorStop(1.0, 'rgb(239,240,242)');
     ctx.lineWidth = 4;
     ctx.strokeStyle = grad;
     ctx.strokeText(text, posx, posy - 3);
@@ -70,10 +74,14 @@ function redraw() {
   //赤
   {
     var grad = ctx.createLinearGradient(0, 20, 0, 100);
-    grad.addColorStop(0, 'rgb(230, 0, 0)');
-    grad.addColorStop(0.5, 'rgb(123, 0, 0)');
-    grad.addColorStop(0.51, 'rgb(240, 0, 0)');
-    grad.addColorStop(1, 'rgb(5, 0, 0)');
+    grad.addColorStop(0, 'rgb(192,204,217)');
+    grad.addColorStop(0.05, 'rgb(212,220,228)');
+    grad.addColorStop(0.15, 'rgb(241,242,243)');
+    grad.addColorStop(0.23, 'rgb(241,242,243)');
+    grad.addColorStop(0.5, 'rgb(190,190,190)');
+    grad.addColorStop(0.51, 'rgb(220,220,221)');
+    grad.addColorStop(0.52, 'rgb(220,220,220)');
+    grad.addColorStop(1.0, 'rgb(239,240,242)');
     ctx.fillStyle = grad;
     ctx.fillText(text, posx, posy - 3);
   }
